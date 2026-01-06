537dfe0b62fd959a40bed15701dcd75ed2c7e4ce
vinhhh <vinhhh@kozo-japan.com>
2026-01-06 11:09:26 +0700

    Handle auth aws-amplify

--- src/configs/amplify.ts
+++ src/configs/amplify.ts
@@ -1,10 +1,10 @@
 const amplifyConfig = {
   Auth: {
     Cognito: {
-      userPoolId: process.env.NEXT_PUBLIC_USER_POOL_ID || 'ap-southeast-1_BXDk3dhbU',
-      userPoolClientId: process.env.NEXT_PUBLIC_CLIENT_ID || '40d5q9prdkl811tenqkj1b9uvs',
-      region: process.env.NEXT_PUBLIC_REGION || 'ap-southeast-1',
-      domain: process.env.NEXT_PUBLIC_DOMAIN || '',
+      userPoolId: process.env.NEXT_PUBLIC_USER_POOL_ID,
+      userPoolClientId: process.env.NEXT_PUBLIC_CLIENT_ID,
+      region: process.env.NEXT_PUBLIC_REGION,
+      domain: process.env.NEXT_PUBLIC_DOMAIN,
       loginWith: {
         email: true,
         password: true,
